74f1fbc853987f58b12c2265b06c789bcb1e587e
Robert Anizoba <robert.anizoba@andela.com>
2019-10-11 16:08:02 +0100

    Add winston and morgan, add logs to .gitignore

---
+++ src/config/winston/index.js
@@ -0,0 +1,38 @@
+const winston = require('winston');
+const appRoot = require('app-root-path');
+
+const { createLogger, transports } = winston;
+
+const options = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 524880,
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+const logger = createLogger({
+  transports: [
+    new transports.File(options.file),
+    new transports.Console(options.console),
+  ],
+  exitOnError: false,
+});
+
+logger.stream = {
+  write: (message, encoding) => {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
