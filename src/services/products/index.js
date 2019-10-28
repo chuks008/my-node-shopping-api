40c53d7ff882a6a8e9ae695bf54b6e13769fe200
Robert Anizoba <robert.anizoba@andela.com>
2019-10-28 09:37:08 +0100

    Create product service and add product service unit tests

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -7,33 +7,158 @@ module.exports = Product => {
      * Add a new product
      *
      * @param {*} productName - name of the new product
-     * @param {*} productCategory - product category of the new product
+     * @param {*} categoryId - product category of the new product
      */
-    addProduct: (productName, productCategory) => {},
+    addProduct: async (productName, categoryId) => {
+      try {
+        const existingProduct = await Product.findOne({
+          where: { productName },
+        });
+
+        if (existingProduct) {
+          throw new Error(
+            'This product already exists. Please use a different product name',
+          );
+        }
+
+        const newProduct = await Product.create({
+          productName,
+          categoryId,
+        });
+
+        if (newProduct) {
+          return {
+            message: 'The product was added successfully',
+            product_name: newProduct.productName,
+            category_id: newProduct.categoryId,
+            error: false,
+          };
+        }
+      } catch (err) {
+        return {
+          message: err.message,
+          error: true,
+        };
+      }
+    },
     /**
      * Get a specific product by using its id
      *
      * @param {*} productId - product id for the product to get
      */
-    getProductById: productId => {},
+    getProductById: async productId => {
+      try {
+        const product = await Product.findOne({
+          where: { id: productId },
+        });
+
+        if (!product) {
+          throw new Error(
+            `No product with id ${productId} was found`,
+          );
+        }
+
+        return {
+          product: {
+            id: product.id,
+            product_name: product.categoryName,
+            category_id: product.categoryId,
+          },
+          error: false,
+        };
+      } catch (err) {
+        return {
+          message: err.message,
+          error: true,
+        };
+      }
+    },
     /**
-     * Get a list of products based on a particular product category id
+     * Get a list of products in db
      *
-     * @param {*} productCategoryId - id for the product category to find
      */
-    getProductsByCategory: productCategoryId => {},
+    getAllProducts: async () => {
+      try {
+        const products = await Product.findAll();
+
+        if (!products) {
+          throw new Error('No products found');
+        }
+
+        const productResult = products.map(product => {
+          return {
+            id: product.id,
+            product_name: product.productName,
+          };
+        });
+
+        return {
+          count: productResult.length,
+          products: productResult,
+          error: false,
+        };
+      } catch (err) {
+        return {
+          message: err.message,
+          error: true,
+        };
+      }
+    },
     /**
      * Update the name of a product
      *
      * @param {*} productId - product id of product to update
      * @param {*} newProductName - new product name after the update
      */
-    updateProduct: (productId, newProductName) => {},
+    updateProduct: async (productId, newProductName) => {
+      try {
+        const updatedProduct = await Product.update(
+          { productName: newProductName },
+          { where: { id: productId } },
+        );
+
+        if (!updatedProduct) {
+          throw new Error("This product does't exist");
+        }
+
+        return {
+          message: `The product was successfully updated to ${newProductName}`,
+          error: false,
+        };
+      } catch (err) {
+        return {
+          message: err.message,
+          error: true,
+        };
+      }
+    },
     /**
      * Delete a product
      *
      * @param {*} productId - id for product to be deleted
      */
-    deleteProduct: productId => {},
+    deleteProduct: async productId => {
+      try {
+        const deleteSuccess = await Product.destroy({
+          where: { id: productId },
+        });
+
+        if (deleteSuccess === 1) {
+          return {
+            message: `Successfully deleted product`,
+            error: false,
+          };
+        }
+
+        throw new Error(
+          'Something went wrong while deleting this product',
+        );
+      } catch (err) {
+        return {
+          message: err.message,
+          error: true,
+        };
+      }
+    },
   };
 };
---
+++ test/unit/services/productService.test.js
@@ -0,0 +1,193 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const assert = require('assert');
+const { Product } = require('../../../src/models').models;
+const sut = require('../../../src/services/products')(Product);
+
+describe('Product tests', () => {
+  describe('Add Product tests', () => {
+    afterEach(() => {
+      Product.findOne.restore();
+    });
+
+    const productName = 'toy';
+    const categoryId = 5;
+    const product = {
+      id: 1,
+      productName: 'toy',
+      categoryId: 5,
+    };
+
+    it('should return an error response if product already exists', async () => {
+      const errorMessage =
+        'This product already exists. Please use a different product name';
+      sinon.stub(Product, 'findOne').resolves(product);
+      const errorResponse = await sut.addProduct(
+        productName,
+        categoryId,
+      );
+      expect(errorResponse.error).to.be.true;
+      expect(errorResponse.message).to.equal(errorMessage);
+    });
+
+    it('should return a success response if the product was successfully created', async () => {
+      sinon.stub(Product, 'findOne').resolves(null);
+      sinon.stub(Product, 'create').resolves(product);
+      const successResponse = await sut.addProduct(
+        productName,
+        categoryId,
+      );
+
+      expect(successResponse).to.contain.key('product_name');
+      expect(successResponse.error).to.be.false;
+      expect(successResponse.message).to.equal(
+        'The product was added successfully',
+      );
+      expect(successResponse.product_name).to.equal(productName);
+      expect(successResponse.category_id).to.equal(categoryId);
+    });
+  });
+
+  describe('Update product tests', () => {
+    const productId = 45;
+    const newProductName = 'Scrabble';
+
+    afterEach(() => {
+      Product.update.restore();
+    });
+
+    it('should return an error response when the product does not exist', async () => {
+      const errorMsg = "This product does't exist";
+      sinon.stub(Product, 'update').resolves(null);
+      const updateResponse = await sut.updateProduct(
+        productId,
+        newProductName,
+      );
+
+      expect(updateResponse.error).to.be.true;
+      assert.equal(updateResponse.message, errorMsg);
+    });
+
+    it('should return a success response when the product was successfully updated', async () => {
+      const successMsg = `The product was successfully updated to ${newProductName}`;
+      const updatedProduct = {
+        id: productId,
+        productName: newProductName,
+      };
+
+      sinon.stub(Product, 'update').resolves(updatedProduct);
+
+      const updateResponse = await sut.updateProduct(
+        productId,
+        newProductName,
+      );
+
+      expect(updateResponse.error).to.be.false;
+      assert.equal(updateResponse.message, successMsg);
+    });
+  });
+
+  describe('Get All Products tests', () => {
+    afterEach(() => {
+      Product.findAll.restore();
+    });
+
+    it('should return an error response when there are no products found', async () => {
+      const errorMsg = 'No products found';
+      sinon.stub(Product, 'findAll').resolves(null);
+      const productResult = await sut.getAllProducts();
+
+      expect(productResult.error).to.be.true;
+      assert.equal(productResult.message, errorMsg);
+    });
+
+    it('should return a success response when products are found', async () => {
+      const productsResponse = [
+        {
+          id: 1,
+          productName: 'Scrabble',
+          categoryId: 7,
+        },
+        {
+          id: 3,
+          productName: 'Playing Cards',
+          categoryId: 2,
+        },
+      ];
+
+      sinon.stub(Product, 'findAll').resolves(productsResponse);
+
+      const productResult = await sut.getAllProducts();
+
+      expect(productResult).contains.keys('products', 'count');
+      expect(productResult.error).to.be.false;
+      expect(productResult.count).to.equal(2);
+      expect(productResult.products[0]).contains.keys('product_name');
+    });
+  });
+
+  describe('Get product by ID tests', () => {
+    afterEach(() => {
+      Product.findOne.restore();
+    });
+
+    const productId = 45;
+
+    it('should return an error response when there is no product found', async () => {
+      const errorMsg = `No product with id ${productId} was found`;
+
+      sinon.stub(Product, 'findOne').resolves(null);
+      const productResult = await sut.getProductById(productId);
+
+      expect(productResult.error).to.be.true;
+      assert.equal(productResult.message, errorMsg);
+    });
+
+    it('should return a success response when a product is found', async () => {
+      const productResponse = {
+        id: 45,
+        name: 'food',
+      };
+
+      sinon.stub(Product, 'findOne').resolves(productResponse);
+
+      const productResult = await sut.getProductById(productId);
+
+      expect(productResult).contains.keys('product');
+      expect(productResult.error).to.be.false;
+      expect(productResult.product).has.keys(
+        'product_name',
+        'id',
+        'category_id',
+      );
+    });
+  });
+
+  describe('Delete product tests', () => {
+    afterEach(() => {
+      Product.destroy.restore();
+    });
+
+    const productId = 45;
+
+    it('should return an error response when a product was not deleted succesfully', async () => {
+      const failureMessage = `Something went wrong while deleting this product`;
+      sinon.stub(Product, 'destroy').resolves(0);
+
+      const deleteResponse = await sut.deleteProduct(productId);
+
+      expect(deleteResponse.error).to.be.true;
+      assert.equal(deleteResponse.message, failureMessage);
+    });
+
+    it('should return a success response when a product is successfully deleted', async () => {
+      const successMessage = `Successfully deleted product`;
+      sinon.stub(Product, 'destroy').resolves(1);
+
+      const deleteResponse = await sut.deleteProduct(productId);
+
+      expect(deleteResponse.error).to.be.false;
+      assert.equal(deleteResponse.message, successMessage);
+    });
+  });
+});
