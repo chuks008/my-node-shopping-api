a30a743965065ea9579f3d34b6164847d75149a3
Robert Anizoba <robert.anizoba@andela.com>
2019-10-28 14:44:16 +0100

    Begin work on product category routes

---
+++ src/routes/products.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+
+// const router = express.Router();
+// const productCategoryController = require('../controllers/productCategory');
+
+// router.get(
+//   '/categories/:id/products',
+//   productCategoryController.getProductsById,
+// );
+
+// module.exports = router;
